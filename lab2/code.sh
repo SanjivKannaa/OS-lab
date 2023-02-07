c6c44f13ddd74a7a7a0bc43aa1133cfebc66d2b6
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-02-07 14:07:24 +0530

    lab2

--- lab2/code.sh
+++ lab2/code.sh
@@ -1,50 +1,62 @@
 #!/bin/bash
 
 # 1. Write a shell script to calculate the value of X^n
-x=0
-n=0
-ans=1
-read -p "X = " x
-read -p "n = " n
-for i in $(seq 1 n)
-do
-    ans=$((ans*x))
-done
-echo "x^n = $(ans)"
+# x=0
+# n=0
+# ans=1
+# read -p "X = " x
+# read -p "n = " n
+# for i in $(seq 1 n)
+# do
+#     ans=$((ans*x))
+# done
+# echo "x^n = $(ans)"
 
 # 2. Write a shell script to read 10 numbers from the user and to find the sum and average of the numbers.
-sum=0
-for i in {1..10}
-do
-    echo "Enter the input"
-    read value
-    sum=$((sum+value))
-done
-echo "The sum is $sum"
+# sum=0
+# for i in {1..10}
+# do
+#     echo "Enter the input"
+#     read value
+#     sum=$((sum+value))
+# done
+# echo "The sum is $sum"
 
 
 # 3. Write a shell script to find whether the given number is Armstrong or not
 
 
 # 4. Write a shell script to print all the divisors of a given integer.
-n=0
-read -p "enter the number" n
-for i in $(seq 1 $n)
-do
-    if [ $((n%i)) -eq 0 ]
-    then
-        echo $i
-    fi
-done
+# n=0
+# read -p "enter the number" n
+# for i in $(seq 1 $n)
+# do
+#     if [ $((n%i)) -eq 0 ]
+#     then
+#         echo $i
+#     fi
+# done
 
 
 # 5. Write a shell script to count the number of vowels in the string.
-
+# count=0
+# s=""
+# vowels="aeiouAEIOU"
+# read -p "enter the string: " s
+# for ((i=0;i<${#s};i++))
+# do
+#     char="${s:$i:1}"
+#     if [[ $vowels =~ $char ]]
+#     then
+#         count=$((count+1))
+#     fi
+# done
+# echo "number of vowels = $count"
 
 # 6. Write a shell script to reverse the string without using in-built functions.
-s=""
-read -p "enter the string: " s
-for (( i=${#s}-1; i>=0; i-- ))
-do
-    echo "${s:$i:1}"
-done
+# s=""
+# read -p "enter the string: " s
+# for (( i=${#s}-1; i>=0; i-- ))
+# do
+#     echo "${s:$i:1}"
+# done
